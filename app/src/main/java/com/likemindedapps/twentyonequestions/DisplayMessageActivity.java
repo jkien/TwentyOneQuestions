4a867a8033c73765df5f619edf8825bf9fdf218b
Jason Kien <kien.jason@gmail.com>
2015-03-08 18:05:48 -0400

    Revert 13967fc..c0a29be
    This rolls back to commit 13967fc19dc6fd37a446775f594b3bea73788181.

--- app/src/main/java/com/likemindedapps/twentyonequestions/DisplayMessageActivity.java
+++ app/src/main/java/com/likemindedapps/twentyonequestions/DisplayMessageActivity.java
@@ -1,8 +1,9 @@
 package com.likemindedapps.twentyonequestions;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
